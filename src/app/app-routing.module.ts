00fa3ffbde211b996a8d5d969daaa31cf6b52eef
StrukovSergei <strukov.sergei.999@gmail.com>
2023-10-17 15:42:20 +0300

    initial routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { RestaurantItemComponent } from './pages/restaurant-item/restaurant-item.component';
+import { CreateOrderComponent } from './pages/create-order/create-order.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'Login',
+    component: LoginComponent
+  },
+  {
+    path: 'foodCategory',
+    component: CategoryComponent
+  },
+  {
+    path: 'restaurantItems',
+    component: RestaurantItemComponent
+  },
+  {
+    path: 'createOrder',
+    component: CreateOrderComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { RestaurantItemComponent } from './pages/restaurant-item/restaurant-item.component';
+import { CreateOrderComponent } from './pages/create-order/create-order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CategoryComponent,
+    RestaurantItemComponent,
+    CreateOrderComponent
   ],
   imports: [
     BrowserModule,
