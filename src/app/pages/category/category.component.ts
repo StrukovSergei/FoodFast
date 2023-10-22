c0282318c3c13edbefc0cf966811164310039808
StrukovSergei <strukov.sergei.999@gmail.com>
2023-10-22 21:04:20 +0300

    category cmp

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MasterService } from 'src/app/services/master.service';
 
 @Component({
-  selector: 'app-category',
-  templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css']
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit{
+
+  categoryList: any[] = [];
+  constructor(private masterSrv: MasterService, private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+    this.loadAllFoodCategories()
+  }
+
+  loadAllFoodCategories() {
+    this.masterSrv.getAllFoodCategory().subscribe((res: any)=> {
+      this.categoryList = res.data;
+    })
+  }
+  navigate(item: string) {
+    this.router.navigate(['/restaurant-items',item])
+  }
+
 
 }
